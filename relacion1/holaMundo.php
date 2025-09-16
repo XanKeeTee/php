b64af44b16f9185d62e002bd07931063c5eaf3b4
XanKeeTee <89795692+XanKeeTee@users.noreply.github.com>
2025-09-16 19:56:12 +0200

    primera subida drag and drop

---
+++ relacion1/holaMundo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Relacion 1 - Hola mundo</title>
+    <link rel="shortcut icon" href="./playamar.png" type="image/x-icon">
+</head>
+<body>
+    <h1>Hola mundo en php</h1>
+    <?php
+        echo 'Hola Mundo!';
+    ?>
+</body>
+</html>
